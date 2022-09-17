993bf44dfae9d465a45a3950b6791d6fc1fcd985
Ramirez <ramirez.alberto@zoho.com>
2022-09-16 19:44:55 -0700

    Configure services and cookie options

--- Clubhouse/Program.cs
+++ Clubhouse/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = ".AspNetCoreCookie";
+        options.SlidingExpiration = true;
+        options.LoginPath = "/Home/Login";
+    });
 
 var app = builder.Build();
 
