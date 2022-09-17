c88df04dc48173bac7c3897d35b922a5ffa4389e
Ramirez <ramirez.alberto@zoho.com>
2022-09-16 22:06:15 -0700

    Add first migration and seed the db

---
+++ Clubhouse/Data/SeedClubhouse.cs
@@ -0,0 +1,28 @@
+﻿using Clubhouse.Models;
+
+namespace Clubhouse.Data
+{
+    public class SeedClubhouse
+    {
+        public static void Initialize(ClubhouseContext context)
+        {
+            if (context.User.Any())
+                return;
+
+            var users = new User[]
+            {
+                new User{UserName="pedrasa1",Password="1234567890",Email="m@gma4.com"},
+                new User{UserName="pedrasa2",Password="1234567890",Email="m@gma3.com"},
+                new User{UserName="pedrasa3",Password="1234567890",Email="m@gma2.com"},
+                new User{UserName="pedrasa4",Password="1234567890",Email="m@gma1.com"}
+            };
+
+            foreach (User user in users)
+            {
+                context.User.Add(user);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ Clubhouse/Migrations/20220917044544_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clubhouse.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
